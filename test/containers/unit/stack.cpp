aaef99e783013be31d52d82fe06fb6a2dfc39e6d
nkapliev <nkapliev@gmail.com>
2017-09-03 12:48:52 +0100

    Added test for stack.size() method.

--- test/containers/unit/stack.cpp
+++ test/containers/unit/stack.cpp
@@ -1,7 +1,26 @@
 #include <gtest/gtest.h>
 #include <nba/containers/stack.hpp>
 
-TEST(StackUnitTest, IsEmpty) {
+TEST(StackUnitTest, Empty) {
     nba::Stack<int> stack;
+
     EXPECT_TRUE(stack.empty());
+    stack.push(1);
+    EXPECT_FALSE(stack.empty());
+}
+
+TEST(StackUnitTest, Size) {
+    nba::Stack<int> stack;
+
+    EXPECT_EQ(stack.size(), 0);
+    stack.push(1);
+    EXPECT_EQ(stack.size(), 1);
+    stack.push(1);
+    EXPECT_EQ(stack.size(), 2);
+    stack.pop();
+    EXPECT_EQ(stack.size(), 1);
+    stack.pop();
+    EXPECT_EQ(stack.size(), 0);
+    stack.pop();
+    EXPECT_EQ(stack.size(), 0);
 }
